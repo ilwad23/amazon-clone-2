0eff81a40b67f57431e5d5822787d9bd13a0b971
Ilwad Abukor <ilwadabukor@hotmail.com>
2022-03-10 14:44:51 +0000

    updated - folder structure

---
+++ Component/Nav.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
+import { ShoppingCartIcon } from "@heroicons/react/outline";
+import { MenuIcon } from "@heroicons/react/outline";
+import { useMediaMatcher } from "../util/hooks";
+function Nav() {
+  const [search, setSearch] = useState(false);
+  const mobile = useMediaMatcher();
+  return (
+    <nav className="nav ">
+      <div className="navTop">
+        <div className={`${mobile && search ? "hidden" : "navTop__logo"}`}>
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            alt="logo"
+            className="cursor-pointer"
+          />
+        </div>
+
+        <div
+          className={`navTop__searchBar ${
+            mobile && search ? "navTop__searchBar-mobile" : " "
+          }`}
+        >
+          <input
+            type="text"
+            className={`navTop__input ${
+              mobile && search ? "flex-1" : mobile ? "hidden" : ""
+            }`}
+            onBlur={(e) => setSearch(false)}
+          />
+          <SearchIcon
+            className="navTop__searchIcon"
+            onClick={() => setSearch(!search)}
+          />
+        </div>
+        <div className={`${mobile && search ? "hidden" : "navTop__links"}`}>
+          <div className="navTop__link">
+            <p>hello, user</p>
+            <h3>Account & List</h3>
+          </div>
+          <div className="navTop__link">
+            <p>Returns</p>
+            <h3>& Order</h3>
+          </div>
+          <div className="navTop__basket">
+            <div className="navTop__iconWrapper">
+              <p className="navTop__num">0</p>{" "}
+              <ShoppingCartIcon className="navTop__icon" />
+            </div>
+            <div className="navTop__link">
+              <h3>Basket</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="navBottom">
+        <div className="navBottom__menu">
+          <MenuIcon className="navBottom__icon" />
+          <p className="">All</p>
+        </div>
+        <div className="navBottom__links">
+          <div className="navBottom__menu">
+            <p className="">Prime Video</p>
+          </div>
+          <div className="navBottom__menu">
+            <p className="">Prime Video</p>
+          </div>
+          <div className="navBottom__menu">
+            <p className="">Prime Video</p>
+          </div>
+          <div className="navBottom__menu">
+            <p className="">Prime Video</p>
+          </div>
+          <div className="navBottom__menu">
+            <p className="">Prime Video</p>
+          </div>
+          <div className="navBottom__menu">
+            <p className="">Prime Video</p>
+          </div>
+          <div className="navBottom__menu">
+            <p className="">Prime Video</p>
+          </div>
+          <div className="navBottom__menu">
+            <p className="">Prime Video</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 import Head from "next/head";
-import { useState } from "react";
-import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
-import { ShoppingCartIcon } from "@heroicons/react/outline";
-import { MenuIcon } from "@heroicons/react/outline";
-import { useMediaMatcher } from "../util/hooks";
+import Nav from "../Component/Nav";
+
 export default function Home() {
-  const [search, setSearch] = useState(false);
-  const mobile = useMediaMatcher();
-  console.log(search);
+
   return (
     <div>
       <Head>
@@ -16,86 +10,7 @@ export default function Home() {
         <meta name="description" content="Amazon clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className="nav ">
-        <div className="navTop">
-          <div className={`${mobile && search ? "hidden" : "navTop__logo"}`}>
-            <Image
-              src="https://links.papareact.com/f90"
-              width={150}
-              height={40}
-              objectFit="contain"
-              alt="logo"
-              className="cursor-pointer"
-            />
-          </div>
-
-          <div
-            className={`navTop__searchBar ${
-              mobile && search? "navTop__searchBar-mobile":" "
-            }`}
-          >
-            <input
-              type="text"
-              className={`navTop__input ${
-                mobile && search ? "flex-1" : mobile ? "hidden":""
-              }`}
-              onBlur={(e) => setSearch(false)}
-            />
-            <SearchIcon className="navTop__searchIcon" onClick={()=>setSearch(!search)} />
-          </div>
-          <div className={`${mobile && search ? "hidden" : "navTop__links"}`}>
-            <div className="navTop__link">
-              <p>hello, user</p>
-              <h3>Account & List</h3>
-            </div>
-            <div className="navTop__link">
-              <p>Returns</p>
-              <h3>& Order</h3>
-            </div>
-            <div className="navTop__basket">
-              <div className="navTop__iconWrapper">
-                <p className="navTop__num">0</p>{" "}
-                <ShoppingCartIcon className="navTop__icon" />
-              </div>
-              <div className="navTop__link">
-                <h3>Basket</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="navBottom">
-          <div className="navBottom__menu">
-            <MenuIcon className="navBottom__icon" />
-            <p className="">All</p>
-          </div>
-          <div className="navBottom__links">
-            <div className="navBottom__menu">
-              <p className="">Prime Video</p>
-            </div>
-            <div className="navBottom__menu">
-              <p className="">Prime Video</p>
-            </div>
-            <div className="navBottom__menu">
-              <p className="">Prime Video</p>
-            </div>
-            <div className="navBottom__menu">
-              <p className="">Prime Video</p>
-            </div>
-            <div className="navBottom__menu">
-              <p className="">Prime Video</p>
-            </div>
-            <div className="navBottom__menu">
-              <p className="">Prime Video</p>
-            </div>
-            <div className="navBottom__menu">
-              <p className="">Prime Video</p>
-            </div>
-            <div className="navBottom__menu">
-              <p className="">Prime Video</p>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Nav />
       <main></main>
     </div>
   );
