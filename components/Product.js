af192f75f63d6d9d1a85caf17565125103c71ad4
Ilwad Abukor <ilwadabukor@hotmail.com>
2022-04-24 10:07:05 +0100

    test - inata link

--- components/Product.js
+++ components/Product.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */ import React from "react";
 import Image from "next/image";
+import Link from "next/Link";
 import { StarIcon } from "@heroicons/react/solid";
 function Product({
   title,
@@ -8,33 +9,40 @@ function Product({
   category,
   rating,
   img,
-  colSpan=1,
+  colSpan = "1",
 }) {
+  const Rating = Math.floor(rating);
+  const Prime = Math.random() > 0.5 ? true : false;
   return (
-    <div className={`product col-span-${colSpan}`}>
-      <p className="product__category">Men’s Collection</p>
+    <div className={`product l:col-span-${colSpan}`}>
+      <p className="product__category">{category}</p>
+      <Link href="https://www.instagram.com/bladerunnerrealworld/">
+
       <img
-        // src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
         src={img}
         alt="product"
         height="200"
         width="200"
         loading="lazy"
         className="mx-auto pt-2"
-      />
+        />
+        </Link>
       <div className="product__info">
-        <h2 className="product__title">
-          {/* Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops */}
-          {title}
-        </h2>
+        <h2 className="product__title">{title}</h2>
         <div className="product__ratings flex ">
-          <StarIcon className="product__starIcon text-amazonYellow w-6 h-6" />
-          <StarIcon className="product__starIcon text-amazonYellow w-6 h-6" />
-          <StarIcon className="product__starIcon text-amazonYellow w-6 h-6" />
+          {Array(Rating)
+            .fill()
+            .map((_, i) => (
+              <StarIcon
+                key={i}
+                className="product__starIcon text-amazonYellow w-6 h-6"
+              />
+            ))}
         </div>
         <p className="product__description line-clamp-2">{description}</p>
-        <p className="product__price">£{price}</p>
-        <div className="product__prime">
+        <p className="product__price py-2">£{price}</p>
+
+        <div className={`product__prime product__prime--${Prime && "toggle"}`}>
           <Image
             src="https://links.papareact.com/fdw"
             width={63}
