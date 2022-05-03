d7cfa283e1a6fe9449c81efaa36643460b038824
Ilwad Abukor <ilwadabukor@hotmail.com>
2022-05-03 16:38:54 +0100

    update - banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,13 +1,38 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import React from "react";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 function Banner() {
   return (
-    <div className="relative">
-      <img src="https://links.papareact.com/gi1" loading="lazy" alt="Banner" />
-      <div className="shadow"></div>
+    <div className="relative  bg-blue-500 ">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-[#E5E5E5] to-transparent z-20 bottom-0"></div>
+
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img
+            src="https://links.papareact.com/6ff"
+            loading="lazy"
+            alt="Banner"
+          />
+        </div>
+        <div>
+          <img
+            src="https://links.papareact.com/gi1"
+            loading="lazy"
+            alt="Banner"
+          />
+        </div>
+      </Carousel>
     </div>
   );
 }
 
-export default Banner
+export default Banner;
